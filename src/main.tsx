98acc5cd6dde9872dd911f6605aa216038ec826a
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-07-01 15:22:14 +0700

    Remove debug line

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,7 @@ import './index.css'
 import { ThemeProvider } from './theme.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/index.ts'
-import Time from './utilities/Time.ts'
 import '../configureAmplify.ts'
-
-const date = new Date(2023, 4, 18, 16, 0, 0).getTime();
-
-console.log(Time.timeFormat(date));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
