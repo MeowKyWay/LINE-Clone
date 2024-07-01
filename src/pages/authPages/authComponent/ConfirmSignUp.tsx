403237f745d655802928f26964e3b6c895330781
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-07-01 15:08:18 +0700

    Update ConfirmSignUp.tsx
    Add step to check is confirm complete

--- src/pages/authPages/authComponent/ConfirmSignUp.tsx
+++ src/pages/authPages/authComponent/ConfirmSignUp.tsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import TextField from "../../../components/input/TextField";
 import Button from "../../../components/input/Button";
 import { confirmSignUp } from "@aws-amplify/auth";
+import Complete from "./Complete";
 
 function ConfirmSignUp({ email }: {
     email: string;
 }) {
 
     const [otp, setOtp] = useState('');
+    const [isComplete, setIsComplete] = useState(false);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -17,26 +19,33 @@ function ConfirmSignUp({ email }: {
             confirmationCode: otp,
         })
 
-        console.log(res);
+        if (res.isSignUpComplete) {
+            setIsComplete(true);
+        }
     }
 
     return (
-        <form className="flex flex-col mt-10 w-full" onSubmit={handleSubmit}>
-            <TextField
-                type="text"
-                value={otp}
-                onChange={setOtp}
-                className="border-0 border-b rounded-b-none"
-                autoComplete="username"
-                name="username">
-                Username
-            </TextField>
-            <Button
-                type={otp ? 'primary' : 'disabled'}
-                className="mt-4">
-                Confirm Sign up
-            </Button>
-        </form>
+        <div>
+            {!isComplete &&
+                <form className="flex flex-col mt-10 w-full" onSubmit={handleSubmit}>
+                    <TextField
+                        type="text"
+                        value={otp}
+                        onChange={setOtp}
+                        className="border-0 border-b rounded-b-none"
+                        autoComplete="username"
+                        name="username">
+                        Username
+                    </TextField>
+                    <Button
+                        type={otp ? 'primary' : 'disabled'}
+                        className="mt-4">
+                        Confirm Sign up
+                    </Button>
+                </form>
+            }
+            {isComplete && <Complete>Sign up complete</Complete>}
+        </div>
     )
 }
 
