354bb2775fada69658236df67f6bfe9585e93369
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-07-02 15:05:06 +0700

    Update Body.tsx
    Fixed bug that header now showing

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import { ReactNode } from "react";
 import Menu from "./Menu";
 import Chat from "./Chat";
+import { RoutePath } from "../RoutePath";
 
 function Body ({children}: {children: ReactNode}) {
 
@@ -10,7 +11,7 @@ function Body ({children}: {children: ReactNode}) {
 
     return (
         <div className="flex flex-col h-full flex-1">
-            {location.pathname==='/chats' && <Header />}
+            {location.pathname===RoutePath.CHATS && <Header />}
             <div className="flex flex-row w-full flex-1">
                 <Menu>
                     {children}
