bac4b8bbd30dfc53407c958d92662138be8ea0a1
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-07-03 10:28:55 +0700

    Update TestPage.tsx
    Styling

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,8 +1,21 @@
 //For testing
 
+import { generateClient } from "aws-amplify/api";
+import useTheme from "../theme";
+
 function TestPage() {
+
+    const theme = useTheme().currentTheme;
+    
+    const client = generateClient();
+
     return (
-        <div className="flex flex-col">
+        <div 
+            className="flex flex-col items-center"
+            style={{
+                color: theme.color.primary.text,
+            }}
+        >
             <h1>Test Page</h1>
         </div>
     )
