d20ba6340be0072601d1305a5683eebbb038fb06
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-07-12 13:12:45 +0700

    Update ProfilePicture.tsx

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,27 +1,22 @@
 import { StorageImage } from "@aws-amplify/ui-react-storage"
 
-function ProfilePicture ({src, size}: {
+function ProfilePicture({ src, size }: {
     src?: string | null
     size: string
 }) {
 
     return (
         <div>
-            {
-                src ? <StorageImage path={src} alt="profile" style={
-                    {
-                        width: size,
-                        height: size,
-                        minWidth: size,
-                        minHeight: size,
-                        borderRadius: '50%',
-                    }
-                }/>
-                :
-                <img 
-                className="object-cover" 
-                src={'../src/assets/profile.jpeg'} 
-                alt={'Profile'} 
+            {src ? <StorageImage path={src} alt="profile" style={{
+                width: size,
+                height: size,
+                minWidth: size,
+                minHeight: size,
+                borderRadius: '50%',
+            }} /> : <img
+                className="object-cover"
+                src={'../src/assets/profile.jpeg'}
+                alt={'Profile'}
                 style={{
                     width: size,
                     height: size,
