18863602e67350dc90945aada661be6d3321385f
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-07-23 12:50:11 +0700

    Remove header

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,17 +1,17 @@
-import { useLocation } from "react-router-dom"
-import Header from "./Header";
+// import { useLocation } from "react-router-dom"
+// import Header from "./Header";
 import { ReactNode } from "react";
 import Menu from "./Menu";
 import ChatPane from "./chat/ChatPane";
-import { RoutePath } from "../RoutePath";
+// import { RoutePath } from "../RoutePath";
 
 function Body ({children}: {children: ReactNode}) {
 
-    const location = useLocation();
+    // const location = useLocation();
 
     return (
         <div className="flex flex-col h-full flex-1">
-            {location.pathname===RoutePath.CHATS && <Header />}
+            {/* {location.pathname===RoutePath.CHATS && <Header />} */}
             <div className="flex flex-row w-full flex-1">
                 <Menu>
                     {children}
